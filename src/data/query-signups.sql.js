d80ec755ddbd30c1d59cb65ffdd5d75ebabd9c89
Julian Winternheimer <julianwinternheimer@gmail.com>
2025-05-20 12:26:39 -0400

    Reorganize file structure

--- src/data/query-signups.sql.js
+++ src/data/query-signups.sql.js
@@ -1,9 +1,11 @@
+export default `
 select
   date(timestamp_trunc(timestamp, week)) as week
-  , count(distinct user_id) as weekly_active_users
-from `buffer-data.dbt_buffer.segment_post_sent`
+  , count(distinct user_id) as signups
+from \`buffer-data.dbt_buffer.segment_accounts_created\`
 where date(timestamp_trunc(timestamp, week)) between
 date_sub(current_date, interval 52 week) and 
 date_sub(current_date, interval 1 week)
 group by 1
 order by 1 asc
+`; 
--- src/data/query-subscription-starts.sql.js
+++ src/data/query-subscription-starts.sql.js
@@ -1,9 +1,11 @@
+export default `
 select
   date(timestamp_trunc(timestamp, week)) as week
   , count(distinct user_id) as new_customers
-from `buffer-data.dbt_buffer.segment_subscription_starts`
+from \`buffer-data.dbt_buffer.segment_subscription_starts\`
 where date(timestamp_trunc(timestamp, week)) between
 date_sub(current_date, interval 52 week) and 
 date_sub(current_date, interval 1 week)
 group by 1 
 order by 1 asc
+`; 
