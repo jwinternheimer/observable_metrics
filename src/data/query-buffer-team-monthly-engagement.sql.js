b28351c439c7e320c08df9a5bf9107e6ef6bea7b
Julian Winternheimer <julianwinternheimer@gmail.com>
2025-06-18 09:30:42 -0400

    Add current streaks

--- src/data/query-buffer-team-monthly-engagement.sql.js
+++ src/data/query-buffer-team-monthly-engagement.sql.js
@@ -6,10 +6,13 @@ with admins as (
         , a.name
         , o.id as organization_id
         , a.id as account_id
+        , s.count as current_streak
     from dbt_buffer.core_accounts a
     inner join dbt_buffer.core_organizations as o
         on a.organization_id = o.id
         and o.is_deleted is not true
+    inner join dbt_buffer.core_organization_streaks as s
+        on o.id = s.organization_id
     where a.is_deleted is not true
     and (a.account_role = 'admin' 
             or a.id in ('64195847e05614e23680fa18','626a09140c7dc67219132e21','5f1f00e8b73dcc2f63797106','5ca1d41e905a7a394986a738'))
@@ -18,7 +21,7 @@ with admins as (
 select
     a.avatar
     , a.name
-    , a.account_id
+    , a.current_streak
     , count(distinct up.id) as posts
     , ifnull(sum(up.likes), 0) as likes
     , safe_add(ifnull(sum(up.reposts), 0), ifnull(sum(up.retweets), 0)) as reposts
--- src/data/query-buffer-team-posts.sql.js
+++ src/data/query-buffer-team-posts.sql.js
@@ -4,9 +4,13 @@ select
   , count(distinct up.id) as posts
   , sum(up.total_engagements) as total_engagement
 from dbt_buffer.publish_updates as up
-inner join dbt_buffer.publish_users as u 
-  on u.id = up.user_id
-  and u.role = 'admin'
+inner join dbt_buffer.core_organizations as o
+  on o.id = up.organization_id
+  and o.is_deleted is not true
+inner join dbt_buffer.core_accounts as a
+  on a.organization_id = o.id
+  and a.is_deleted is not true
+  and a.account_role = 'admin'
 where date(timestamp_trunc(up.sent_at, week)) between
     date_sub(current_date, interval 52 week) and 
     date_sub(current_date, interval 1 week)
